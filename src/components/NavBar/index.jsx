2b69773467dafd2b3685b2df4d470169f83d17dc
Mairajk <mairajkhan597@gmail.com>
2022-09-28 21:01:54 +0500

    working........2

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -4,14 +4,14 @@ import {
     Routes,
     Route,
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
 import "./style.css"
 
-import  Home  from "../Home/index";
-import  Groups  from "../Groups";
-import  Friends  from "../Friends";
-import  Games  from "../Games";
-import  Videos  from "../Videos";
+import Home from "../Home/index";
+import Groups from "../Groups";
+import Friends from "../Friends";
+import Games from "../Games";
+import Videos from "../Videos";
 
 // import fbLogo from './images/fb logo.png'
 // import profilePhoto from './images/dp.jpg'
@@ -31,67 +31,78 @@ import ToggleFunction from './ToggleButton/toggle';
 
 let NavBar = () => (
 
-    
-<div>    
+
+    <div>
         <Router>
-    <nav className='navBar'>
+            <nav className='navBar'>
 
-        <div className='left'>
+                <div className='left'>
 
-            <div>
-                {/* <img src={fbLogo} alt="" className='facebookLogo' /> */}
-            </div>
+                    <div>
+                        {/* <img src={fbLogo} alt="" className='facebookLogo' /> */}
+                    </div>
 
-            <div className='inputDiv'>
-                <label htmlFor="search">
-                    <i className="searchLogo leftLogo navLogo"><BiSearchAlt2 /></i>
-                </label>
-                <input type="search" placeholder='Search Facebook' />
-            </div>
+                    <div className='inputDiv'>
+                        <label htmlFor="search">
+                            <i className="searchLogo leftLogo navLogo"><BiSearchAlt2 /></i>
+                        </label>
+                        <input type="search" placeholder='Search Facebook' />
+                    </div>
 
-        </div>
-
-
-
-            <div className="center">
-
-                <ul>
-
-                    <li><Link to="/">
-                        <i className='homeLogo centerLogo navLogo'><VscHome /></i>
-                    </Link></li>
-
-                    <li><Link to="/Friends">
-                        <i className='friendsLogo centerLogo navLogo'><BsFillPeopleFill /></i>
-                    </Link></li>
-
-                    <li><Link to="/Videos">
-                        <i className='watchLogo centerLogo navLogo'><MdOutlineOndemandVideo /></i>
-                    </Link></li>
-
-                    <li><Link to="/Groups">
-                        <i className='groupsLogo centerLogo navLogo'><HiUserGroup /></i>
-                    </Link></li>
-
-                    <li><Link to="/Games">
-                        <i className='gamingLogo centerLogo navLogo'><SiFacebookgaming /></i>
-                    </Link></li>
-
-                </ul>
-
-            </div>
+                </div>
 
 
-        <div className="right">
-            <ToggleFunction />
-            <div><i className="menuLogo rightLogo navLogo"><TbGridDots /></i></div>
-            <div><i className="messengerLogo rightLogo navLogo"><RiMessengerFill /></i></div>
-            <div><i className="bellLogo rightLogo navLogo"><BsFillBellFill /></i></div>
-            {/* <div> <img className="navProfilePhoto rightLogo navLogo " src={profilePhoto} alt="" /></div> */}
-        </div>
 
-    </nav>
-    <Routes>
+                <div className="center">
+
+                    <ul>
+
+                        <li>
+                            <Link to="/">
+                                <i className='homeLogo centerLogo navLogo'><VscHome /></i>
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/Friends">
+                                <i className='friendsLogo centerLogo navLogo'><BsFillPeopleFill /></i>
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/Videos">
+                                <i className='watchLogo centerLogo navLogo'><MdOutlineOndemandVideo /></i>
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/Groups">
+                                <i className='groupsLogo centerLogo navLogo'><HiUserGroup /></i>
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/Games">
+                                <i className='gamingLogo centerLogo navLogo'><SiFacebookgaming /></i>
+                            </Link>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+
+                <div className="right">
+                    <ToggleFunction />
+                    <div><i className="menuLogo rightLogo navLogo"><TbGridDots /></i></div>
+                    <div><i className="messengerLogo rightLogo navLogo"><RiMessengerFill /></i></div>
+                    <div><i className="bellLogo rightLogo navLogo"><BsFillBellFill /></i></div>
+                    {/* <div> <img className="navProfilePhoto rightLogo navLogo " src={profilePhoto} alt="" /></div> */}
+                </div>
+
+            </nav>
+
+            <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="Friends" element={<Friends />} />
                 <Route path="Groups" element={<Groups />} />
@@ -101,7 +112,7 @@ let NavBar = () => (
 
         </Router>
 
-</div>
+    </div>
 );
 
 export default NavBar;
